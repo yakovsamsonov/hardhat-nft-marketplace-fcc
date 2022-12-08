38740258cead2671e4371a4333fb33174b6be0ec
Yakov <yasamsonov@gmail.com>
2022-12-08 13:09:16 +0300

    Create NftMarketplace solidity contract

---
+++ contracts/NftMarketplace.sol
@@ -0,0 +1,99 @@
+// 24.01.10
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+error NftMarketplace__PriceMustBeAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotOwner();
+
+contract NftMarketplace {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // mapping NFT Token Address => NFT Token ID => Listing (Price, Seller)
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    modifier NotListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price <= 0) {
+            revert NftMarketplace__NotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    /*
+     * @notice Method for listing your NFT on the Marketplace
+     * @param nftAddress: Address of an NFT
+     * @param tokenId: The token ID of an NFT (uint256)
+     * @param price: sale price of the listed NFT (uint256)
+     * @dev Technically we could have the contart to be escrow for the NFTS
+     * but this way people still hold their NFT when listed
+     */
+
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    ) external NotListed(nftAddress, tokenId, msg.sender) isOwner(nftAddress, tokenId, msg.sender) {
+        if (price <= 0) {
+            revert NftMarketplace__PriceMustBeAboveZero();
+        }
+
+        IERC721 nft = IERC721(nftAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketplace__NotApprovedForMarketplace();
+        }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId
+    ) external payable isListed(nftAddress, tokenId) {}
+}
+
+// 1. `listItem`: List NFT on the Marketplace
+// 2. `buyItem`: Buy the NFT
+// 3. `cancelItem`: Cancel listing on Marketplace
+// 4. `updateListing`: Update price
+// 5. `withdrawProceeds`: Withdraw payment for my bought NFTs
